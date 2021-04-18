3fd52aaf426c2c591c7201e7840f7bb5f726cf78
robinsrk <robinsrk3@gmail.com>
2021-04-18 22:27:58 +0600

    update

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const MongoClient = require("mongodb").MongoClient;
 const app = express();
-const fileUpload = require("express-fileupload");
 const port = 3300;
+const ObjectId = require("mongodb").ObjectId;
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static("services"));
-app.use(fileUpload());
 
 require("dotenv").config();
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.emnwf.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
@@ -31,6 +29,15 @@ client.connect((err) => {
   const packageCollection = client
     .db(process.env.DB_NAME)
     .collection(process.env.DB_PAC);
+  const reviewCollection = client
+    .db(process.env.DB_NAME)
+    .collection(process.env.DB_REV);
+  const ordersCollection = client
+    .db(process.env.DB_NAME)
+    .collection(process.env.DB_ORD);
+  const adminCollection = client
+    .db(process.env.DB_NAME)
+    .collection(process.env.DB_ADMIN);
   console.log("connected");
   app.get("/services", (req, res) => {
     serviceCollection.find({}).toArray((err, documents) => {
@@ -46,6 +53,58 @@ client.connect((err) => {
     packageCollection.find({}).toArray((err, documents) => {
       res.send(documents);
     });
+  });
+  app.get("/reviews", (req, res) => {
+    reviewCollection.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+  app.get("/package/:id", (req, res) => {
+    packageCollection
+      .find({ _id: ObjectId(req.params.id) })
+      .toArray((err, documents) => {
+        res.send(documents);
+      });
+  });
+  app.post("/placeOrder", (req, res) => {
+    const order = req.body;
+    ordersCollection.insertOne(order).then((result) => {
+      console.log(result);
+    });
+  });
+  app.post("/addReview", (req, res) => {
+    const review = req.body;
+    reviewCollection.insertOne(review);
+  });
+  app.post("/addAdmin", (req, res) => {
+    const admin = req.body;
+    adminCollection.insertOne(admin);
+  });
+  app.post("/orderHistory", (req, res) => {
+    const email = req.body.email;
+    adminCollection.find({ adminEmail: email }).toArray((err, admin) => {
+      let filter = {};
+      if (admin.length === 0) {
+        filter.email = email;
+      }
+      ordersCollection.find(filter).toArray((err, documents) => {
+        res.send(documents);
+      });
+    });
+  });
+  app.post("/isAdmin", (req, res) => {
+    const email = req.body.email;
+    console.log(email);
+    adminCollection.find({ adminEmail: email }).toArray((err, admin) => {
+      res.send(admin.length > 0);
+    });
+  });
+  app.delete("/delete/:id", (req, res) => {
+    serviceCollection.deleteOne({ _id: ObjectId(req.params.id) });
+  });
+  app.post("/addService", (req, res) => {
+    const service = req.body;
+    serviceCollection.insertOne(service);
   });
 });
 app.listen(process.env.PORT || port);
